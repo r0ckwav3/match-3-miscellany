39c8d6960ae8b55931b19a883a4d362a77cd80c0
r0ckwav3 <peterrockwave@gmail.com>
2022-08-17 09:19:18 -0700

    added optional rotation offset

--- RegularNgon.py
+++ RegularNgon.py
@@ -1,17 +1,17 @@
 import pygame
 import math
 
-def get_ngon_points(center, radius, n):
+def get_ngon_points(center, radius, n, rot_offset=0):
     output = []
     for i in range(n):
-        d = (math.cos((i/n)*2*math.pi),math.sin((i/n)*2*math.pi))
+        d = (math.cos(rot_offset + (i/n)*2*math.pi),math.sin(rot_offset + (i/n)*2*math.pi))
         pos = (center[0] + d[0]*radius, center[1] + d[1]*radius)
         output.append(pos)
     return output
 
-def draw_ngon(screen, color, center, radius, n):
+def draw_ngon(screen, color, center, radius, n, rot_offset=0):
     pygame.draw.polygon(
         screen,
         color,
-        get_ngon_points(center, radius, n)
+        get_ngon_points(center, radius, n, rot_offset)
     )
